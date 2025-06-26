c6cfcaac90ef74462e4b363b219dd6fe5281a74c
Alvaro Leiva <aleivag@gmail.com>
2025-06-26 11:05:06 -0700

    release (and test) v0.13.4

---
+++ tests/test_version.py
@@ -0,0 +1,17 @@
+from pathlib import Path
+import toml
+
+from cstq import Query
+
+def test_version():
+    """all version should be the same"""
+    root = Path(__file__).parent.parent
+    pyproject = root / "pyproject.toml"
+    setup = root / "setup.py"
+    version = root / "pystemd/__version__.py"
+
+    setup_version = Query(setup).find_function_call(func_name="setup").extended_node().keyword_args["version"].literal_eval()
+    version_version = Query(version).find_assignment("__version__").value.literal_eval_for_node()
+    pyproject_version = toml.loads(pyproject.read_text())["project"]["version"]
+    assert setup_version == version_version == pyproject_version
+
